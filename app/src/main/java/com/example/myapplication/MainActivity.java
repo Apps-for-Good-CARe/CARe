078103b5ddfdbcf9528024c1fbbbe492ecbcda21
adrew <awlee@wpi.edu>
2021-04-01 09:21:41 -0400

    hithere

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-//yeeaeaaAndreww
+//yeeaeaaAndrewww
 public class MainActivity extends AppCompatActivity {
 
     @Override
